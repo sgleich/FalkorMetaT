fcf7e52874d7d93c5409e41ea07b2dd57c864482
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2022-11-09 17:01:19 -0800

    Create Figures.R

---
+++ Figures.R
@@ -0,0 +1,5 @@
+### This script will allow you to make some plots using the normalized, metatranscriptome data ###
+### By: Samantha Gleich ###
+### Last Updated: November 9, 2022 ###
+
+COMING SOON
