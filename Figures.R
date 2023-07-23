d87193032d80fb79c10adb64b333bc257b1a5b59
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-07-23 14:03:32 -0700

    Delete Figures.R

--- Figures.R
+++
@@ -1,5 +0,0 @@
-### This script will allow you to make some plots using the normalized, metatranscriptome data ###
-### By: Samantha Gleich ###
-### Last Updated: November 9, 2022 ###
-
-COMING SOON
