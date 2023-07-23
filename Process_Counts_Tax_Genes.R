bddf7863b1a5de5dd8ae07eaa2d84a25d17761dc
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-07-23 14:02:25 -0700

    Update Process_Counts_Tax_Genes.R

--- Process_Counts_Tax_Genes.R
+++ Process_Counts_Tax_Genes.R
@@ -1,6 +1,6 @@
-### This script will allow you to combine the salmon (count data), EUKulele (taxonomy data), eggNOG (function/gene data), and ortholog group (MMSeqs data) metaT outputs into one wide dataframe. This wide dataframe will be noramlized via (NormalizeData.R)###
+### This script will allow you to combine the salmon (count data), EUKulele (taxonomy data), eggNOG (function/gene data), and ortholog group (MMSeqs data) metaT outputs into one wide dataframe. This wide dataframe will be noramlized via (NormalizeData.R) ###
 ### By: Samantha Gleich ###
-### Last Updated: November 9, 2022 ###
+### Last Updated: July 23, 2023 ###
 
 # Load libraries
 library(tidyverse)
@@ -16,21 +16,27 @@ tax_fin <- read.csv("EUKulele_compiled.csv",header=TRUE,row.names=1)
 tax_fin2 <- data.frame(tax_fin$contigID, tax_fin$Taxonomy)
 colnames(tax_fin2) <- c("contigID","Taxonomy")
 
-# Load in gene (i.e. functional annotation) information 
+# Load in gene (i.e., functional annotation) information 
 eggnog_fin <-read.csv("eggnog_final.csv",header=TRUE,row.names=1)
 
-# Join all of the metaT outputs together. We can "join" the dataframes by the contigID.
+# Join eukulele, eggnog, and salmon outputs together. We can "join" the dataframes by the contigID.
 tax.ko <- full_join(tax_fin2,eggnog_fin)
 tax.ko.count <- left_join(tax.ko, salmon_fin2)
 tax.ko.count <- subset(tax.ko.count, NumReads !=0)
 
+# Load in cluster number (i.e., MMSeqs data)
+clus <- read.delim("clusters.tsv", header=FALSE)
+colnames(clus) <- c("contigID","Cluster")
+clus$Cluster <- as.factor(clus$Cluster)
+
+# Join cluster information with taxonomy, function, and salmon counts
+all <- left_join(tax.ko.count,clus)
+
 # Now we can save our data in long format (but we'll really want the data in wide format)
-# write.csv(tax.ko.count, "long_format_data.csv")
+# write.csv(all, "long_format_data.csv")
 
 # Let's wrangle the data so that it's in wide format
-wide.data <- dcast(tax.ko.count, contigID+Taxonomy+KO+OG+KO2~SampleName,fun.aggregate = sum,value.var= "NumReads")
-wide.data$OG <- NULL
-wide.data$KO <- NULL
+wide.data <- dcast(all, contigID+Taxonomy+KO+Cluster~SampleName,fun.aggregate = sum,value.var= "NumReads")
 
 # Save wide format data
 write.csv(wide.data,"wide_format_data.csv")
