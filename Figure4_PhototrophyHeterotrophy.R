87813ee1aeb588210eadbbcac4ab0b9d59e5455f
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-07-25 16:35:16 -0700

    Update Figure4_PhototrophyHeterotrophy.R

--- Figure4_PhototrophyHeterotrophy.R
+++ Figure4_PhototrophyHeterotrophy.R
@@ -1,6 +1,6 @@
 ### Figure 4 - Phototrophy/Heterotrophy plots ###
 ### This script will allow you to summarize and plot the average +/- SE of various phototrophy/heterotrophy biomarker gene groups given a set of normalized transcript abundances ###
-### Last Updated: July 24, 2023 ###
+### Last Updated: July 25, 2023 ###
 
 # Load libraries 
 library(ggplot2)
@@ -21,34 +21,25 @@ df <- subset(df,KO!="")
 
 # Sum counts per KO terms for each sample
 dfSum <- df %>% group_by(KO) %>% summarize_all(sum) %>% as.data.frame()
-####
-
-# Melt dataframe to make it long format
-dfMelt <- melt(dfSum,id.vars="KO")
-
-# Create new columns with eddy type and depth
-cols <- colsplit(dfMelt$variable,"\\.",c("X","Eddy","Depth","Rep"))
-dfMelt$Eddy <- cols$Eddy
-dfMelt$Depth <- cols$Depth
 
 # Gene groups - This is a list of KO terms that were defined as "phototrophy-associated" and "heterotrophy-associated" in this study
 ko <- read.csv("KEGG.csv",header=TRUE)
 
 # Join transcript abundance data and KO term list
-dfMelt <- left_join(dfMelt,ko)
+dfSum <- left_join(dfSum,ko)
 
 # Depth plot function
 # Input: Dataframe containing transcript abundances and KO term list info, target gene group (column in KO list dataframe), and title of plot
 # Output: Plot of mean Z-score transformed gene group abundance +/- SE across all eddy type/depth combinations
 depthPlot <- function(df,target,title){
   df <- subset(df,Biomarker.Gene.Group==paste(target))
-  df$KEGG.KO <- NULL
+  df$KO <- NULL
   df <- df[c(1:24)]
   dfMelt <- melt(df)
-  dfSum <- dfMelt %>% group_by(variable) %>% summarize(sumz=sum(value)) %>% as.data.frame()
-  m <- mean(dfSum$sumz)
-  s <-sd(dfSum$sumz)
-  dfZ <- dfSum %>% group_by(variable) %>% summarize(z=(sumz-m)/s)
+  dfTot <- dfMelt %>% group_by(variable) %>% summarize(sumz=sum(value)) %>% as.data.frame()
+  m <- mean(dfTot$sumz)
+  s <-sd(dfTot$sumz)
+  dfZ <- dfTot %>% group_by(variable) %>% summarize(z=(sumz-m)/s)
   colz <- colsplit(dfZ$variable,"\\.",c("x","eddy","depth","rep"))
   dfZ$eddy <- colz$eddy
   dfZ$depth <- colz$depth
@@ -62,19 +53,19 @@ depthPlot <- function(df,target,title){
   return(plot)}
 
 # Implement function for the 12 gene groups focused on in this study
-fat <- depthPlot(df,"Fatty acid breakdown","Fatty acid breakdown")
-calvin <- depthPlot(df,"Calvin cycle","Calvin cycle")
-n <- depthPlot(df,"Inorganic N uptake and assimilation","Inorganic N uptake + assimilation")
-endo <- depthPlot(df,"Endocytosis","Endocytosis")
-auto <- depthPlot(df,"Autophagy","Autophagy")
-lyso <- depthPlot(df,"Lysosome","Lysosome")
-cath <- depthPlot(df,"Cathepsin","Cathepsin Protease")
-vtype <- depthPlot(df,"V-type ATPase","V-type ATPase")
-photo <- depthPlot(df,"Photosynthesis","Photosynthesis")
-phago <- depthPlot(df,"Phagosome","Phagosome")
-rho <- depthPlot(df,"Rho GTPase","Rho GTPase")
-ant <- depthPlot(df,"Antenna proteins","Antenna proteins")
+fat <- depthPlot(dfSum,"Fatty acid breakdown","Fatty acid breakdown")
+calvin <- depthPlot(dfSum,"Calvin cycle","Calvin cycle")
+n <- depthPlot(dfSum,"Inorganic N uptake and assimilation","Inorganic N uptake + assimilation")
+endo <- depthPlot(dfSum,"Endocytosis","Endocytosis")
+auto <- depthPlot(dfSum,"Autophagy","Autophagy")
+lyso <- depthPlot(dfSum,"Lysosome","Lysosome")
+cath <- depthPlot(dfSum,"Cathepsin","Cathepsin Protease")
+vtype <- depthPlot(dfSum,"V-type ATPase","V-type ATPase")
+photo <- depthPlot(dfSum,"Photosynthesis","Photosynthesis")
+phago <- depthPlot(dfSum,"Phagosome","Phagosome")
+rho <- depthPlot(dfSum,"Rho GTPase","Rho GTPase")
+ant <- depthPlot(dfSum,"Antenna proteins","Antenna proteins")
 
 # Combine all plots together with a common legend and add panel labels (a-l)
-photo+calvin+n+ant+phago+vtype+fat+cath+rho+endo+lyso+auto+plot_layout(guides = "collect",nrow=3)+plot_annotation(tag_levels="a")
-# ggsave("Figure4.pdf",width=15,height=8)
+photo+calvin+n+ant+phago+vtype+fat+cath+rho+endo+lyso+auto+plot_layout(guides = "collect",nrow=3)+plot_annotation(tag_levels="a") 
+# ggsave("../../Figure4.pdf",width=15,height=8)
