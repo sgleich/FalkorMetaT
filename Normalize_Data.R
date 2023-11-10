841ca7a414922c0cad7d66fe3ff92b91534de2dc
Samantha Gleich <64509848+sgleich@users.noreply.github.com>
2023-11-10 12:55:53 -0500

    Update Normalize_Data.R

--- Normalize_Data.R
+++ Normalize_Data.R
@@ -1,6 +1,6 @@
 ### This script will allow you to normalize transcript abundance data across samples using the edgeR TMM CPM method. ###
 ### By: Samantha Gleich ###
-### Last Updated: August 1, 2023 ###
+### Last Updated: November 10, 2023 ###
 
 # Load libraries
 library(edgeR)
@@ -8,13 +8,12 @@ library(edgeR)
 # Load in wide format data (compiled but not yet normalized; obtained via Compile_Data.R)
 df <- read.csv("wide_format_data.csv",header=TRUE,row.names=1)
 df <- subset(df,grepl("Eukaryota",df$Taxonomy))
-df$Cluster <- as.character(df$Cluster)
-df$KO <- as.character(df$KO)
+df$KEGG <- as.character(df$KEGG)
 
-df$KO <- ifelse(is.na(df$KO),"",df$KO)
+df$KO <- ifelse(is.na(df$KEGG),"",df$KEGG)
 
-# Set up edgeR list. Counts are salmon sample counts, genes are identifiers (i.e. contigID, KO, Cluster, and Taxonomy), groups are you telling edgeR which groups exist in your dataset (i.e. sample types)
-dge.metaT <- DGEList(counts=df[5:28],genes=df[1:4],group=c(rep("Cyclonic-DCM",3),rep("Cyclonic-25m",3),rep("Anticyclonic-250m",3),rep("Anticyclonic-150m",3),rep("Anticyclonic-DCM",3),rep("Anticyclonic-25m",3),rep("Cyclonic-250m",3),rep("Cyclonic-150m",3)))
+# Set up edgeR list. Counts are salmon sample counts, genes are identifiers (i.e. Name, KEGG, and Taxonomy), groups are you telling edgeR which groups exist in your dataset (i.e. sample types)
+dge.metaT <- DGEList(counts=df[4:27],genes=df[1:3],group=c(rep("Cyclonic-DCM",3),rep("Cyclonic-25m",3),rep("Anticyclonic-250m",3),rep("Anticyclonic-150m",3),rep("Anticyclonic-DCM",3),rep("Anticyclonic-25m",3),rep("Cyclonic-250m",3),rep("Cyclonic-150m",3)))
 
 # Optional check to see if groupings look good
 # dge.metaT$samples
